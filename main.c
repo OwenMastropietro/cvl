71a762c9931e787afc4617d0f644ad00648f7915
OwenMastropietro <owen.mastroj@gmail.com>
2026-02-11 18:41:42 -0800

    add example usage

---
+++ main.c
@@ -0,0 +1,28 @@
+// Example Program: Interfacing with CVL and NetPBM.
+
+#include "cvl/cvl.h"
+#include "netpbm/netpbm.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    Image img = readImage("./data/original/car.ppm");
+
+    cvl_threshold(&img, 128);
+    writeImage(img, "./data/modified/car_bw.pbm");
+
+    cvl_add_noise(&img, 0.1);
+    writeImage(img, "./data/modified/car_bw_noisy.pbm");
+
+    cvl_shrink(&img);
+    cvl_expand(&img);
+    cvl_expand(&img);
+    cvl_shrink(&img);
+    writeImage(img, "./data/modified/car_bw_cleared.pbm");
+
+    deleteImage(img);
+
+    return 0;
+}
